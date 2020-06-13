6563f2e45556e0214ead2c6ddb802ddf6b721b0a
BitMasher <thebitmasher@gmail.com>
2020-06-12 20:01:57 -0500

    fixed user id column size to account for ksuid

--- backend/dbmigrations/20200221231558_initialize.up.sql
+++ backend/dbmigrations/20200221231558_initialize.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
-    id           varchar(16) NOT NULL,
+    id           varchar(27) NOT NULL,
     display_name varchar(32) NULL,
     "name"       varchar(64) NULL,
     dob          date        NULL,
@@ -11,7 +11,7 @@ CREATE TABLE user_oauth_providers
 (
     id       varchar(256) NOT NULL,
     provider varchar(32)  NOT NULL,
-    user_id  varchar(16)  NOT NULL,
+    user_id  varchar(27)  NOT NULL,
     CONSTRAINT user_oauth_provider_un UNIQUE (user_id),
     CONSTRAINT user_oauth_provider_pk PRIMARY KEY (id, provider)
 );
